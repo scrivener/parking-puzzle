9ca05658ff34deafa99760c0437efc6400fc05ed
Ada Lerner <ada.r.lerner@gmail.com>
2022-01-03 16:43:08 -0500

    Add a second level to levels.c

--- src/levels.c
+++ src/levels.c
@@ -35,3 +35,37 @@ outcome level1(Level *level) {
 
     return ok;
 }
+
+outcome level2(Level *level) {
+    level->numPiecesInStage = 4;
+
+    level->pieces[0].topLeft.x = 0;
+    level->pieces[0].topLeft.y = 2;
+    level->pieces[0].o = H;
+    level->pieces[0].length = 2;
+
+    level->pieces[1].topLeft.x = 5;
+    level->pieces[1].topLeft.y = 0;
+    level->pieces[1].o = V;
+    level->pieces[1].length = 3;
+
+    level->pieces[2].topLeft.x = 3;
+    level->pieces[2].topLeft.y = 5;
+    level->pieces[2].o = H;
+    level->pieces[2].length = 3;
+
+    level->pieces[3].topLeft.x = 2;
+    level->pieces[3].topLeft.y = 4;
+    level->pieces[3].o = V;
+    level->pieces[3].length = 2;
+
+    level->numPiecesInStage = 4;
+
+    level->goal.x = 6;
+    level->goal.y = 2;
+    
+    strcpy(level->victoryPhrase, "YAS QUEEN");
+    strcpy(level->topPhrase, "Escape Parking Hell");
+
+    return ok;
+}
